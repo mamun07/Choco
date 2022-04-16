3714884036764fd82a4b22d2ef8dd2e3565d137c
mamun07 <mamunben7@gmail.com>
2022-04-16 16:06:06 +0600

    server port commet

--- server/index.js
+++ server/index.js
@@ -2,11 +2,9 @@ import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
-
 import PostRouters from "./routes/posts.js";
 
 // Mongoose DB use
-const PORT = 5000;
 const userName = "choco";
 const userPass = "choco12345";
 const closter = "Cluster0";
@@ -19,6 +17,9 @@ app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 app.use(express.json());
 app.use(express());
+
+// Server port
+const PORT = 5000;
 app.listen(PORT, () => console.log(`Server Running at ${PORT}`));
 
 app.use("/posts", PostRouters);
