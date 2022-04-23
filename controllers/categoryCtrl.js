57887b3aa5cfbf312d0d1a17da3581fdd199f646
mamun07 <mamunben7@gmail.com>
2022-04-23 15:24:45 +0600

    category create, update, delete working

--- controllers/categoryCtrl.js
+++ controllers/categoryCtrl.js
@@ -11,7 +11,39 @@ export const getCategories = async (req, res) => {
 
 export const creatCategory = async (req, res) => {
   try {
+    // if user have role = 1 (this is admin)......
+    // Only admin can create, delete and update category.......
+    const { name } = req.body;
+    const category = await Categorys.findOne({ name });
+    if (category)
+      return res.status(400).json({ msg: "This category already exist." });
+
+    const newCategory = new Categorys({ name });
+
+    await newCategory.save();
+
     res.json("Check admin working successfully !");
+  } catch (err) {
+    return res.status(500).json({ msg: err.message });
+  }
+};
+
+export const deleteCategory = async (req, res) => {
+  try {
+    await Categorys.findByIdAndDelete(req.params.id);
+
+    res.json({ msg: "Deleted a category." });
+  } catch (err) {
+    return res.status(500).json({ msg: err.message });
+  }
+};
+
+export const updateCategory = async (req, res) => {
+  try {
+    const { name } = req.body;
+    await Categorys.findByIdAndUpdate({ _id: req.params.id }, { name });
+
+    res.json({ msg: "Updated a category." });
   } catch (err) {
     return res.status(500).json({ msg: err.message });
   }
--- routes/categoryRouter.js
+++ routes/categoryRouter.js
@@ -1,6 +1,11 @@
 import express from "express";
 const router = express.Router();
-import { getCategories, creatCategory } from "../controllers/categoryCtrl.js";
+import {
+  getCategories,
+  creatCategory,
+  deleteCategory,
+  updateCategory,
+} from "../controllers/categoryCtrl.js";
 import { auth } from "../middleware/auth.js";
 import { authAdmin } from "../middleware/authAdmin.js";
 
@@ -8,5 +13,10 @@ router
   .route("/category")
   .get(getCategories)
   .post(auth, authAdmin, creatCategory);
+
+router
+  .route("/category/:id")
+  .delete(auth, authAdmin, deleteCategory)
+  .put(auth, authAdmin, updateCategory);
 
 export default router;
