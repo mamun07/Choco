984ee810c4927a891218c747829d1038441b8782
mamun07 <mamunben7@gmail.com>
2022-04-24 16:30:50 +0600

    image upload and delete complete

--- routes/uploadRouter.js
+++ routes/uploadRouter.js
@@ -1,6 +1,7 @@
 import cloudinary from "cloudinary";
 import { auth } from "../middleware/auth.js";
 import { authAdmin } from "../middleware/authAdmin.js";
+import fs from "fs";
 import express from "express";
 
 const router = express.Router();
@@ -12,37 +13,56 @@ cloudinary.config({
   api_secret: process.env.CLOUD_API_SECRET,
 });
 
-// Upload image
-router.post("/upload", (req, res) => {
+// Upload image only admin can use
+router.post("/upload", auth, authAdmin, (req, res) => {
   try {
     console.log(req.files);
     if (!req.files || Object.keys(req.files).length === 0)
-      return res.status(400).send({ msg: "No files were uploaded." });
+      return res.status(400).json({ msg: "No files were uploaded." });
 
-    const file = req.files;
-    if (file.size > 1024 * 1024)
+    const file = req.files.file;
+    if (file.size > 1024 * 1024) {
+      removeTmp(file.tempFilePath);
       return res.status(400).json({ msg: "size too large." });
+    }
 
-    if (
-      file.mimetype !== "image/jpeg" &&
-      file.mimetype !== "image/png" &&
-      file.mimetype !== "image/jpg"
-    )
+    if (file.mimetype !== "image/jpeg" && file.mimetype !== "image/png") {
+      removeTmp(file.tempFilePath);
       return res.status(400).json({ msg: "File format is incorrect." });
+    }
 
     cloudinary.v2.uploader.upload(
       file.tempFilePath,
       { folder: "test" },
       async (err, result) => {
         if (err) throw err;
-        res.json({ result });
+        removeTmp(file.tempFilePath);
+        res.json({ public_id: result.public_id, url: result.secure_url });
       }
     );
-
-    res.json("test upload");
   } catch (err) {
     return res.status(500).json({ msg: err.message });
   }
 });
+
+router.post("/destroy", auth, authAdmin, (req, res) => {
+  try {
+    const { public_id } = req.body;
+    if (!public_id) return res.status(400).json({ msg: "No images selected" });
+    cloudinary.v2.uploader.destroy(public_id, async (err, result) => {
+      if (err) throw err;
+      res.json({ msg: "Deleted image" });
+    });
+  } catch (err) {
+    return res.status(500).json({ msg: err.message });
+  }
+});
+
+// Remove image path from tmp folder
+const removeTmp = (path) => {
+  fs.unlink(path, (err) => {
+    if (err) throw err;
+  });
+};
 
 export default router;
