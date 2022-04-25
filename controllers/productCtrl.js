9c09735f1f90a3dd1557a6e20c6ee37665084a1f
mamun07 <mamunben7@gmail.com>
2022-04-25 15:45:24 +0600

    product create done

--- controllers/productCtrl.js
+++ controllers/productCtrl.js
@@ -2,26 +2,45 @@ import Products from "../models/productModel.js";
 
 export const getProducts = async (req, res) => {
   try {
-    res.json({ msg: "Get Products" });
+    const products = await Products.find();
+    res.json(products);
   } catch (err) {
     return res.status(500).json({ msg: err.message });
   }
 };
-export const createProducts = async (req, res) => {
+export const createProduct = async (req, res) => {
   try {
-    res.json({ msg: "Create Products" });
+    const { product_id, title, price, description, content, images, category } =
+      req.body;
+    if (!images) return res.status(400).json({ msg: "No image upload." });
+
+    const product = await Products.findOne({ product_id });
+    if (product)
+      return res.status(400).json({ msg: "This product already exists." });
+
+    const newProduct = new Products({
+      product_id,
+      title: title.toLowerCase(),
+      price,
+      description,
+      content,
+      images,
+      category,
+    });
+    await newProduct.save();
+    res.json({ msg: "Create a product successfully !" });
   } catch (err) {
     return res.status(500).json({ msg: err.message });
   }
 };
-export const deleteProducts = async (req, res) => {
+export const deleteProduct = async (req, res) => {
   try {
     res.json({ msg: "Delete Products" });
   } catch (err) {
     return res.status(500).json({ msg: err.message });
   }
 };
-export const updateProducts = async (req, res) => {
+export const updateProduct = async (req, res) => {
   try {
     res.json({ msg: "Update Products" });
   } catch (err) {
--- routes/productRouter.js
+++ routes/productRouter.js
@@ -1,9 +1,15 @@
 import express from "express";
+import {
+  getProducts,
+  createProduct,
+  deleteProduct,
+  updateProduct,
+} from "../controllers/productCtrl.js";
 
 const router = express.Router();
 
-router.route("/products").get().post();
+router.route("/products", getProducts).get(getProducts).post(createProduct);
 
-router.route("/products/:id").delete().put();
+router.route("/products/:id").delete(deleteProduct).put(updateProduct);
 
 export default router;
