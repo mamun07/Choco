bcdcec87ee0a96a9ccee9aed7f624489f0896347
mamun07 <mamunben7@gmail.com>
2022-05-16 15:54:54 +0600

    loding message set done

--- client/src/components/mainpage/products/Products.jsx
+++ client/src/components/mainpage/products/Products.jsx
@@ -2,24 +2,28 @@ import React, { useContext } from 'react';
 import { Box, Container, Grid } from '@mui/material';
 import { GlobalState } from '../../../GlobalState';
 import ProductItem from '../utils/productItem/ProductItem';
+import Loding from '../utils/Loding';
 
 const Products = () => {
   const state = useContext(GlobalState);
   const [products] = state.ProductsAPI.products;
   return (
-    <Box my={5}>
-      <Container maxWidth="xl">
-        <Grid container spacing={2}>
-          {products.map((product) => {
-            return (
-              <Grid item xs={6} xl={3} key={product._id}>
-                <ProductItem product={product} />
-              </Grid>
-            );
-          })}
-        </Grid>
-      </Container>
-    </Box>
+    <>
+      <Box my={5}>
+        <Container maxWidth="xl">
+          <Grid container spacing={2}>
+            {products.map((product) => {
+              return (
+                <Grid item xs={6} xl={3} key={product._id}>
+                  <ProductItem product={product} />
+                </Grid>
+              );
+            })}
+          </Grid>
+        </Container>
+      </Box>
+      {products.length === 0 && <Loding />}
+    </>
   );
 };
 
---
+++ client/src/components/mainpage/utils/Loding.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Container, Typography } from '@mui/material';
+
+const Loding = () => {
+  return (
+    <Container maxWidth="xl" sx={{ marginBottom: 10 }}>
+      <Typography variant="h2" textAlign={'center'}>
+        Loging...
+      </Typography>
+    </Container>
+  );
+};
+
+export default Loding;
