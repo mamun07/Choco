12c5c927189bf481d3540406fd49dfb3e581251f
mamun07 <mamunben7@gmail.com>
2022-05-16 17:25:17 +0600

    login page working

--- client/src/components/mainpage/auth/Login.jsx
+++ client/src/components/mainpage/auth/Login.jsx
@@ -1,11 +1,59 @@
-import React from 'react';
-import { Typography } from '@mui/material';
+import React, { useState } from 'react';
+import { Button, TextField, Box } from '@mui/material';
+import axios from 'axios';
 
 const Login = () => {
+  const [user, setUser] = useState({
+    email: '',
+    password: '',
+  });
+
+  const onChangeInput = (e) => {
+    const { name, value } = e.target;
+    setUser({ ...user, [name]: value });
+  };
+
+  const loginSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      await axios.post('/user/login', { ...user });
+
+      localStorage.setItem('firstLogin', true);
+
+      window.location.href = '/';
+    } catch (err) {
+      alert(err.response.data.msg);
+    }
+  };
+
   return (
-    <>
-      <Typography variant="h4">User Login</Typography>
-    </>
+    <Box>
+      <form onSubmit={loginSubmit}>
+        <TextField
+          label="Email"
+          name="email"
+          variant="standard"
+          type="email"
+          required
+          value={user.email}
+          onChange={onChangeInput}
+          sx={{ mb: 3, width: '100%' }}
+        />
+        <TextField
+          label="Password"
+          name="password"
+          variant="standard"
+          type="password"
+          required
+          value={user.password}
+          onChange={onChangeInput}
+          sx={{ mb: 5, width: '100%' }}
+        />
+        <Button type="submit" variant="contained" sx={{ p: '10px 30px' }}>
+          Login
+        </Button>
+      </form>
+    </Box>
   );
 };
 
