4fc0b8ce0095ee57dbdf3532fff603e77213b817
mamun07 <mamunben7@gmail.com>
2022-05-18 11:26:20 +0600

    product grid in home page

--- client/src/components/mainpage/home/Home.jsx
+++ client/src/components/mainpage/home/Home.jsx
@@ -1,70 +1,17 @@
-import React, { useContext } from 'react';
+import React from 'react';
 import { Box, Container, Typography } from '@mui/material';
-import { GlobalState } from '../../../GlobalState';
-import ProductItem from '../utils/productItem/ProductItem';
-import { Swiper, SwiperSlide } from 'swiper/react';
-// import required modules
-import { Autoplay, Navigation } from 'swiper';
+import HomeProductSlider from '../utils/home_product_slider/HomeProductSlider';
 
 const Home = () => {
-  const state = useContext(GlobalState);
-  const [products] = state.ProductsAPI.products;
   return (
     <Box my={5}>
       <Container maxWidth="xl">
-        <Box mb={2}>
-          <Typography variant="h5">NEW ARRIVALS</Typography>
+        <Box mb={4}>
+          <Typography variant="h2">NEW ARRIVALS</Typography>
         </Box>
       </Container>
       <Container maxWidth="xl">
-        <Swiper
-          spaceBetween={15}
-          slidesPerView={5}
-          breakpoints={{
-            320: {
-              slidesPerView: 1,
-              spaceBetween: 15,
-            },
-            375: {
-              slidesPerView: 2,
-              spaceBetween: 15,
-            },
-            425: {
-              slidesPerView: 2,
-              spaceBetween: 15,
-            },
-            640: {
-              slidesPerView: 2,
-              spaceBetween: 15,
-            },
-            768: {
-              slidesPerView: 4,
-              spaceBetween: 15,
-            },
-            1024: {
-              slidesPerView: 5,
-              spaceBetween: 15,
-            },
-          }}
-          autoplay={{
-            delay: 2500,
-            disableOnInteraction: false,
-          }}
-          pagination={{
-            clickable: true,
-          }}
-          navigation={true}
-          modules={[Autoplay, Navigation]}
-          className="mySwiper"
-        >
-          {products.map((product) => {
-            return (
-              <SwiperSlide key={product._id}>
-                <ProductItem product={product} />
-              </SwiperSlide>
-            );
-          })}
-        </Swiper>
+        <HomeProductSlider />
       </Container>
     </Box>
   );
---
+++ client/src/components/mainpage/utils/home_product_slider/HomeProductSlider.jsx
@@ -0,0 +1,63 @@
+import React, { useContext } from 'react';
+import { GlobalState } from '../../../../GlobalState';
+import ProductItem from '../../utils/productItem/ProductItem';
+import { Swiper, SwiperSlide } from 'swiper/react';
+// import required modules
+import { Autoplay, Navigation } from 'swiper';
+
+const HomeProductSlider = () => {
+  const state = useContext(GlobalState);
+  const [products] = state.ProductsAPI.products;
+  return (
+    <Swiper
+      spaceBetween={15}
+      slidesPerView={5}
+      breakpoints={{
+        320: {
+          slidesPerView: 1,
+          spaceBetween: 15,
+        },
+        375: {
+          slidesPerView: 2,
+          spaceBetween: 15,
+        },
+        425: {
+          slidesPerView: 2,
+          spaceBetween: 15,
+        },
+        640: {
+          slidesPerView: 2,
+          spaceBetween: 15,
+        },
+        768: {
+          slidesPerView: 4,
+          spaceBetween: 15,
+        },
+        1024: {
+          slidesPerView: 5,
+          spaceBetween: 15,
+        },
+      }}
+      autoplay={{
+        delay: 2500,
+        disableOnInteraction: false,
+      }}
+      pagination={{
+        clickable: true,
+      }}
+      navigation={true}
+      modules={[Autoplay, Navigation]}
+      className="product-slider"
+    >
+      {products.map((product) => {
+        return (
+          <SwiperSlide style={{ padding: '1px' }} key={product._id}>
+            <ProductItem product={product} />
+          </SwiperSlide>
+        );
+      })}
+    </Swiper>
+  );
+};
+
+export default HomeProductSlider;
